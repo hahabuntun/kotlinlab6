7b3bdde4e6e1cbe3725f1e8b9b97ec0daf89149d
hahabuntun <hahabuntun@gmail.com>
2024-10-29 00:19:37 +0300

    used group by function

--- app/src/main/java/com/example/lab6kotlin/t3.kt
+++ app/src/main/java/com/example/lab6kotlin/t3.kt
@@ -61,12 +61,23 @@ fun main() {
 //    fullMenu.forEach {
 //        println(it)
 //    }
-    val softBakedMenu = cookies.filter {
-        it.softBaked
-    }
+//    val softBakedMenu = cookies.filter {
+//        it.softBaked
+//    }
+//    println("Soft cookies:")
+//    softBakedMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: listOf()
+    val crunchyMenu = groupedMenu[false] ?: listOf()
     println("Soft cookies:")
     softBakedMenu.forEach {
         println("${it.name} - $${it.price}")
     }
-
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
 }
