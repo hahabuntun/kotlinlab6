d410c28e84e85f53c639363b06cf08295bd9b27b
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:25:24 +0300

    added ext method for printing student progress

--- app/src/main/java/com/example/lab6kotlin/t1.kt
+++ app/src/main/java/com/example/lab6kotlin/t1.kt
@@ -22,6 +22,14 @@ class Quiz {
 val Quiz.StudentProgress.progressText: String
     get() = "${answered} of ${total} answered"
 
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+
 fun main(){
     println(Quiz.progressText)
+    Quiz.printProgressBar()
 }
