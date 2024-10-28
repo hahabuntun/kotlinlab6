5528bbe5ecbb9559e8a9730794afc1f82a95af0b
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:37:03 +0300

    created arrays

---
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -0,0 +1,7 @@
+package com.example.lab6kotlin
+
+fun main(){
+    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+
+}
