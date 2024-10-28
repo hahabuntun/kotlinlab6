72c5aa489f0f1e1dc3140928b284cff04871017a
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:30:07 +0300

    replaced extension methods and added interface

--- app/src/main/java/com/example/lab6kotlin/t1.kt
+++ app/src/main/java/com/example/lab6kotlin/t1.kt
@@ -7,8 +7,22 @@ data class Question<T>(
     val answer: T,
     val difficulty: Difficulty
 )
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
 
-class Quiz {
+class Quiz : ProgressPrintable {
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
@@ -19,16 +33,6 @@ class Quiz {
     }
 }
 
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} answered"
-
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) { print("▓") }
-    repeat(Quiz.total - Quiz.answered) { print("▒") }
-    println()
-    println(Quiz.progressText)
-}
-
-fun main(){
-    Quiz.printProgressBar()
+fun main() {
+    Quiz().printProgressBar()
 }
