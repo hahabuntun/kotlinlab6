736deda4fa1fcb9959b4e3148f6554b869a02c39
hahabuntun <hahabuntun@gmail.com>
2024-10-29 00:08:35 +0300

    added map to the program

--- app/src/main/java/com/example/lab6kotlin/t2.kt
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -45,16 +45,30 @@ fun main(){
 //    println(solarSystem.contains("Pluto"))
 //    println("Future Moon" in solarSystem)
 
-    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    println(solarSystem.size)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
     println(solarSystem.size)
-    solarSystem.add("Pluto")
+    solarSystem["Pluto"] = 5
     println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
-    solarSystem.remove("Pluto")
-    println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+    println(solarSystem["Pluto"])
 
 
 }
