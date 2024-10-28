e085f1355632ab08235ab8bfdee2321d37928bfb
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:43:42 +0300

    index of function

--- app/src/main/java/com/example/lab6kotlin/t2.kt
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -23,6 +23,8 @@ fun main(){
     println(solarSystem.size)
     println(solarSystem[2])
     println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
 
 
 }
