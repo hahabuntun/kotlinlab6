5aff7651941febb9473ef2fddc9a395b47b2a941
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:24:02 +0300

    added extension method to quiz

--- app/src/main/java/com/example/lab6kotlin/t1.kt
+++ app/src/main/java/com/example/lab6kotlin/t1.kt
@@ -19,6 +19,9 @@ class Quiz {
     }
 }
 
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+
 fun main(){
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+    println(Quiz.progressText)
 }
