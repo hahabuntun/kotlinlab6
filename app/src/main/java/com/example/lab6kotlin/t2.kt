12b501c6fd69ba62055cfe791db401f8ecacd0bc
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:38:03 +0300

    accessed elements from array

--- app/src/main/java/com/example/lab6kotlin/t2.kt
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -4,4 +4,12 @@ fun main(){
     val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
     val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
     val solarSystem = rockPlanets + gasPlanets
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
 }
