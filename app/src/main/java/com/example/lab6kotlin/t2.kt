a1f82db5efffc56e203aef94fb88411eccdfd35b
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:45:24 +0300

    made list mutable

--- app/src/main/java/com/example/lab6kotlin/t2.kt
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -19,14 +19,20 @@ fun main(){
 //    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
 //    println(newSolarSystem[8])
 
-    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
-    println(solarSystem.size)
-    println(solarSystem[2])
-    println(solarSystem.get(3))
-    println(solarSystem.indexOf("Earth"))
-    println(solarSystem.indexOf("Pluto"))
+//    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    println(solarSystem.size)
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//    println(solarSystem.indexOf("Earth"))
+//    println(solarSystem.indexOf("Pluto"))
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
+
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
     for (planet in solarSystem) {
         println(planet)
     }
-
 }
