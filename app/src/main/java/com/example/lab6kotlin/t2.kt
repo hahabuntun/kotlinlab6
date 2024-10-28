5da866c222ffc4d68021e2ce181da4440852a552
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:37:40 +0300

    added two arrays

--- app/src/main/java/com/example/lab6kotlin/t2.kt
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -3,5 +3,5 @@ package com.example.lab6kotlin
 fun main(){
     val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
     val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
-
+    val solarSystem = rockPlanets + gasPlanets
 }
