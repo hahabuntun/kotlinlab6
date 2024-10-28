d7944303c2c4521776cba3b7e7a8259bb20cb709
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:44:09 +0300

    iterated over elements and printed them:

--- app/src/main/java/com/example/lab6kotlin/t2.kt
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -25,6 +25,8 @@ fun main(){
     println(solarSystem.get(3))
     println(solarSystem.indexOf("Earth"))
     println(solarSystem.indexOf("Pluto"))
-
+    for (planet in solarSystem) {
+        println(planet)
+    }
 
 }
