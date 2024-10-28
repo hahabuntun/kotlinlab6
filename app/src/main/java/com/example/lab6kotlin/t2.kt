35cabcd83c4a296a389ba04a3e486fa90d26dfa2
hahabuntun <hahabuntun@gmail.com>
2024-10-28 23:43:10 +0300

    printed items at specific indices

--- app/src/main/java/com/example/lab6kotlin/t2.kt
+++ app/src/main/java/com/example/lab6kotlin/t2.kt
@@ -21,6 +21,8 @@ fun main(){
 
     val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
     println(solarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.get(3))
 
 
 }
