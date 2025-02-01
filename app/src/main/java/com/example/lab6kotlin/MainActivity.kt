a56a47e106371417b90e991e07e0ae8c78e45f56
hahabuntun <hahabuntun@gmail.com>
2025-02-01 15:39:35 +0300

    HARDWORK-2 fdfs

--- app/src/main/java/com/example/lab6kotlin/MainActivity.kt
+++ app/src/main/java/com/example/lab6kotlin/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab6kotlin.ui.theme.Lab6kotlinTheme
 
+///
+safdsdf
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
